6025e143653f658870e83773ebe9b157edc257f0
youcef23061983 <youcef.belcourtois@gmail.com>
2024-12-02 10:58:56 +0100

    json update

--- back-end/server.js
+++ back-end/server.js
@@ -1,11 +1,54 @@
+// this.post("/login", (schema, request) => {
+//   const { email, password } = JSON.parse(request.requestBody);
+//   const foundUser = schema.users.findBy({ email, password });
+
+//   if (!foundUser) {
+//     return new Response(
+//       401,
+//       {},
+//       { message: "No user with those credentials found!" }
+//     );
+//   }
+
+//   foundUser.password = undefined;
+//   return {
+//     user: foundUser,
+//     token: "Enjoy your pizza, here's your tokens.",
+//   };
+// });
 const jsonServer = require("json-server");
 const server = jsonServer.create();
 const router = jsonServer.router("menu.json");
 const middlewares = jsonServer.defaults();
 
 server.use(middlewares);
+
+// Custom /login route
+server.post("/login", (req, res) => {
+  // Parse the request body
+  const { email, password } = req.body;
+
+  // Access users from the database
+  const db = router.db; // Access lowdb instance
+  const foundUser = db.get("users").find({ email, password }).value();
+
+  if (!foundUser) {
+    return res.status(401).json({
+      message: "No user with those credentials found!",
+    });
+  }
+
+  // Do not send the password back
+  const { password: _, ...userWithoutPassword } = foundUser;
+
+  return res.json({
+    user: userWithoutPassword,
+    token: "Enjoy your pizza, here's your tokens.",
+  });
+});
+
+// CORS Headers
 server.use((req, res, next) => {
-  console.log("Request received");
   res.header("Access-Control-Allow-Origin", "*");
   res.header(
     "Access-Control-Allow-Methods",
@@ -17,24 +60,9 @@ server.use((req, res, next) => {
   );
   next();
 });
-this.post("/login", (schema, request) => {
-  const { email, password } = JSON.parse(request.requestBody);
-  const foundUser = schema.users.findBy({ email, password });
 
-  if (!foundUser) {
-    return new Response(
-      401,
-      {},
-      { message: "No user with those credentials found!" }
-    );
-  }
-
-  foundUser.password = undefined;
-  return {
-    user: foundUser,
-    token: "Enjoy your pizza, here's your tokens.",
-  };
-});
+// Use the default JSON Server router
+server.use(router);
 
 server.use(router);
 server.listen(3000, () => {

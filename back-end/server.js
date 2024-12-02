54ff2b3890b120b6199a8148c95faf02a1c4c5bd
youcef23061983 <youcef.belcourtois@gmail.com>
2024-12-02 10:33:45 +0100

    menu server commit

--- back-end/server.js
+++ back-end/server.js
@@ -1,6 +1,6 @@
 const jsonServer = require("json-server");
 const server = jsonServer.create();
-const router = jsonServer.router("hotel.json");
+const router = jsonServer.router("menu.json");
 const middlewares = jsonServer.defaults();
 
 server.use(middlewares);

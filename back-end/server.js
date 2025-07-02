3ccc3dc06aedc41f552225c6349832e115ea5ebb
youcef23061983 <youcef.belcourtois@gmail.com>
2025-07-02 23:30:00 +0100

    prisma commit19

--- back-end/server.js
+++ back-end/server.js
@@ -42,7 +42,7 @@ app.use(
   })
 );
 
-app.use(express.json());
+app.use(express.json({ limit: "10kb" })); // Limit request size
 if (process.env.NODE_ENV === "development") {
   app.use(morgan("dev")); // Colorful logs
 } else {

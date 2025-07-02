3978c170664a7c15f7dca7c9e7f835f5edae8380
youcef23061983 <youcef.belcourtois@gmail.com>
2025-07-02 18:03:50 +0100

    package commit2

--- front-end/vite.config.js
+++ front-end/vite.config.js
@@ -4,6 +4,8 @@ import react from "@vitejs/plugin-react-swc";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "./", // 👈 ADD THIS LINE
+
   build: {
     outDir: "dist", // This should match Vercel's default static file folder
   },

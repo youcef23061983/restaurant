0450cd1ea53264d7e26d3bab48c2cb41f2eb0d7f
youcef23061983 <youcef.belcourtois@gmail.com>
2025-07-03 08:09:27 +0100

    hemlet commit2

--- back-end/libs/arctjet.js
+++ back-end/libs/arctjet.js
@@ -7,13 +7,11 @@ const aj = (async () => {
 
   return arcjet({
     key: process.env.ARCJET_KEY,
-    characteristics: ["ip.src"],
+    characteristics: ["ip.src", "header.user-agent", "header.accept-language"],
     rules: [
       shield({ mode: "LIVE" }),
-
       detectBot({
         mode: process.env.NODE_ENV === "development" ? "DRY_RUN" : "LIVE",
-        // mode: "LIVE",
         allow: ["CATEGORY:SEARCH_ENGINE"],
       }),
       tokenBucket({
@@ -27,34 +25,3 @@ const aj = (async () => {
 })();
 
 module.exports = aj;
-
-// (async () => {
-//   const arcjetModule = await import("@arcjet/node");
-//   const arcjet = arcjetModule.default;
-//   const { shield, detectBot, tokenBucket } = arcjetModule;
-
-//   const aj = arcjet({
-//     key: process.env.ARCJET_KEY,
-//     characteristics: ["ip.src"],
-//     rules: [
-//       // shield protects your app from common attacks e.g. SQL injection, XSS, CSRF attacks
-//       shield({ mode: "LIVE" }),
-//       detectBot({
-//         mode: "LIVE",
-//         // block all bots except search engines
-//         allow: [
-//           "CATEGORY:SEARCH_ENGINE",
-//           // see the full list at https://arcjet.com/bot-list
-//         ],
-//       }),
-//       // rate limiting
-//       tokenBucket({
-//         mode: "LIVE",
-//         refillRate: 30,
-//         interval: 5,
-//         capacity: 20,
-//       }),
-//     ],
-//   });
-//   module.exports = aj;
-// })();
